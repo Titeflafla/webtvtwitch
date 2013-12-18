fb3b658668aacf3bf29eaf15ba17992bad63e555
Titeflafla <yann.boily@hotmail.fr>
2013-12-18 16:07:17 +0100

    ajout du fichier help

---
+++ help/french/Webtvtwitch.php
@@ -0,0 +1,25 @@
+<?php header('Content-type: text/html; charset=iso-8859-1'); ?><html>
+<head>
+<title>Aides Administration</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body style="font-family: Verdana; font-size: 10px" link="#000000" vlink="#000000" alink="#000000" bgcolor="#FFFFFF">
+<center><big><b>Administration Webtv Twitch</b></big></center><br>
+<li><b>Ajouter une TV</b>
+<br><br>
+<b>Vous devez enregistrer chaque chaine sur http://www.twitch.tv/settings?section=applications sinon le statut ne fonctionnera pas !</b>
+<br><br>
+Name : Statut chaine par exemple
+<br>
+Redirect URI : url de votre site http://www.mon.site/index.php?file=Webtv
+<br><br>
+<b>Titre de la chaine Twitch</b> : Le nom de votre chaine http://www.twitch.tv/<b>lenomdemachaine</b>
+<br><br>
+<b>Votre code Client ID</b> : Il se trouve sur http://www.twitch.tv/settings?section=applications
+<br><br>
+<b>Code flash de la TV</b> : Copier le code de partage &lt;object&gt;&lt;/object&gt; de votre chaine
+<br><br>
+<b>Code HTML du Chan Irc</b> : Copier le code de partage &lt;iframe&gt;&lt;/iframe&gt; pour le chat
+
+<br><br></body></html>
